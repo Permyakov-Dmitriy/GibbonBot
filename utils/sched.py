79c9cedbbea77c5cd1c36c4019d538d7eb350512
Permyakov-Dmitriy <dima.perm11@gmail.com>
2023-07-20 17:26:59 +0600

    update sched module schedule job

--- utils/sched.py
+++ utils/sched.py
@@ -9,22 +9,20 @@ def timer_sched(sched, name, func, bot, id):
     day = format_sch[0]
     HH_MM = format_sch[1]
 
+    print(func)
+
+    dict_day = {
+        'пн': schedule.every().monday,
+        'вт': schedule.every().tuesday,
+        'ср': schedule.every().wednesday,
+        'чт': schedule.every().thursday,
+        'пт': schedule.every().friday,
+        'сб': schedule.every().saturday,
+        'вс': schedule.every().sunday,
+    }
+
     for i in range(len(day)):
-        match day[i]:
-            case "пн":
-                schedule.every().monday.at(HH_MM[i]).do(lambda: func(bot, id)).tag(f"timer{name}")
-            case "вт":
-                schedule.every().tuesday.at(HH_MM[i]).do(lambda: func(bot, id)).tag(f"timer{name}")
-            case "ср":
-                schedule.every().wednesday.at(HH_MM[i]).do(lambda: func(bot, id)).tag(f"timer{name}")
-            case "чт":
-                schedule.every().thursday.at(HH_MM[i]).do(lambda: func(bot, id)).tag(f"timer{name}")
-            case "пт":
-                schedule.every().friday.at(HH_MM[i]).do(lambda: func(bot, id)).tag(f"timer{name}")
-            case "сб":
-                schedule.every().saturday.at(HH_MM[i]).do(lambda: func(bot, id)).tag(f"timer{name}")
-            case "вс":
-                schedule.every().sunday.at(HH_MM[i]).do(lambda: func(bot, id)).tag(f"timer{name}")
+        dict_day[day[i]].at(HH_MM[i]).do(lambda: func(bot, id)).tag(f"timer{name}")
 
 
 def schedule_checker():
