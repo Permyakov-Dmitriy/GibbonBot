6ecf1d65c352b3884494bc8203c23c2ca0d96ba8
Permyakov-Dmitriy <dima.perm11@gmail.com>
2023-07-23 02:46:50 +0600

    update sched.py

--- utils/sched.py
+++ utils/sched.py
@@ -2,12 +2,22 @@ import schedule
 import time
 
 
-def timer_sched(sched, name, func, bot, id):
+def format_for_sched(sched):
     sch = sched.split()
     format_sch = [i.split('/') if '/' in i else i.split() for i in sch]
 
     day = format_sch[0]
     HH_MM = format_sch[1]
+
+    return (day, HH_MM)
+
+
+def timer_sched(sched, name, func, bot, id_grp, format=None):
+    if format == None:
+        format = format_for_sched(sched)
+
+    elif not format[0]:
+        return
 
     dict_day = {
         'пн': schedule.every().monday,
@@ -19,8 +29,9 @@ def timer_sched(sched, name, func, bot, id):
         'вс': schedule.every().sunday,
     }
 
-    for i in range(len(day)):
-        dict_day[day[i]].at(HH_MM[i]).do(lambda: func(bot, id, name)).tag(f"timer{name}")
+    dict_day[format[0][0]].at(format[1][0]).do(lambda: func(bot, id_grp, name)).tag(f"timer_{name}")
+
+    timer_sched(sched, name, func, bot, id_grp, [i[1:] for i in format])
 
 
 def schedule_checker():
